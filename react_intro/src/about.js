2b18449b5a01e9f9435c0182175feebed39c4f93
Vannah-22 <vannjackson16@gmail.com>
2021-03-04 20:30:03 -0700

    first commit

---
+++ react_intro/src/about.js
@@ -0,0 +1,10 @@
+function About() {
+    return( // rendering the content in it //
+        // we need our elements wrapped in parent containers, aka divs//
+        <div> 
+            <h1> Learn more about us </h1>
+            <p> This is our first react app! </p>
+        </div>
+    );
+}
+export default About; // created a file that App.js can reference. //
